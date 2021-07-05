b7a28066a5913fed7ba8d7fcd2a70e242a6bc532
Fajarullah <frullah12@gmail.com>
2021-07-05 21:37:17 +0700

    improve create data page

--- tambah.php
+++ tambah.php
@@ -3,9 +3,7 @@
 require "./koneksi.php";
 require "./helper.php";
 
-$db = new Database();
-
-$employees = $db->find_all("SELECT * FROM pegawai");
+$data = (object)null;
 
 ?>
 
@@ -20,32 +18,8 @@ $employees = $db->find_all("SELECT * FROM pegawai");
       <a class="btn btn-danger" href="index.php">Kembali</a>
     </section>
 
-    
     <form action="proses_tambah.php" method="post">
-      <div class="mb-3">
-        <label for="nip">NIP</label>
-        <input class="form-control" type="text" name="nip" pattern="[0-9]{18}" placeholder="Nomor Induk Pegawai">
-      </div>
-
-      <div class="mb-3">
-        <label for="nama">Nama</label>
-        <input class="form-control" type="text" name="nama" required pattern="[a-zA-Z\. ]{1,64}">
-      </div>
-
-      <div class="mb-3">
-        <label for="jenis_kelamin">Jenis kelamin</label>
-        <select class="form-select" type="text" name="jenis_kelamin" value="l">
-          <option value="">Pilih jenis kelamin</option>
-          <option value="l">Laki-laki</option>
-          <option value="p">Perempuan</option>
-        </select>
-      </div>
-
-      <div class="mb-3">
-        <label for="tanggal_lahir">Tanggal lahir</label>
-        <input class="form-control" type="date" name="tanggal_lahir">
-      </div>
-
+      <? include "./templates/pegawai/form.php" ?>
       <button class="btn btn-primary" type="submit">Tambah</button>
     </form>
   </main>
---
+++ templates/pegawai/form.php
@@ -0,0 +1,23 @@
+<div class="mb-3">
+  <label for="nip">NIP</label>
+  <input value="<?= $data->nip ?>" class="form-control" type="text" name="nip" pattern="[0-9]{18}" placeholder="Nomor Induk Pegawai">
+</div>
+
+<div class="mb-3">
+  <label for="nama">Nama</label>
+  <input value="<?= $data->nama ?>" class="form-control" type="text" name="nama" required pattern="[a-zA-Z\. ]{1,64}">
+</div>
+
+<div class="mb-3">
+  <label for="jenis_kelamin">Jenis kelamin</label>
+  <select class="form-select" data-selected="<?= $data->jenis_kelamin ?>" type="text" name="jenis_kelamin" value="l">
+    <option value="">Pilih jenis kelamin</option>
+    <option value="l">Laki-laki</option>
+    <option value="p">Perempuan</option>
+  </select>
+</div>
+
+<div class="mb-3">
+  <label for="tanggal_lahir">Tanggal lahir</label>
+  <input value="<?= $data->tanggal_lahir ?>" class="form-control" type="date" name="tanggal_lahir">
+</div>
